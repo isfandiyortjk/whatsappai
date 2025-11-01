ad31be4676ada4b3b30bb127d701c527e76a19d9
isfandiyortjk <82015682+isfandiyortjk@users.noreply.github.com>
2025-11-01 17:21:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const app = express();
 app.use(express.json());
 app.use("/webhook", router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Assistant Doner Home running on :${PORT}`));
